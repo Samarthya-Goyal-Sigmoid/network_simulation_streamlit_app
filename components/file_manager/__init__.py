b858a2fa28e662341c8171213c75b918aa5767d8
Samarthya Goyal <samarthya@Samarthyas-MacBook-Air.local>
2025-09-18 12:56:49 +0530

    Add production build folder for deployment

--- components/file_manager/__init__.py
+++ components/file_manager/__init__.py
@@ -1,7 +1,11 @@
 import os
 import streamlit.components.v1 as components
 
-file_manager = components.declare_component(
-    "file_manager",
-    url="http://localhost:3000"
+# Absolute path to frontend/build
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend", "build")
+
+# Declare the component
+file_manager_component = components.declare_component(
+    "file_manager", path=build_dir  # This uses the built frontend
 )
