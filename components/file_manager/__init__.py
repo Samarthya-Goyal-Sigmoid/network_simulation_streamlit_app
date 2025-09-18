90b1f95d2b4ab24d42e02a0bf799cf1ac83e55f0
Samarthya Goyal <samarthya@Samarthyas-MacBook-Air.local>
2025-09-18 13:10:32 +0530

    Include built custom component and deployment setup

--- components/file_manager/__init__.py
+++ components/file_manager/__init__.py
@@ -6,6 +6,6 @@ parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend", "build")
 
 # Declare the component
-file_manager_component = components.declare_component(
+file_manager = components.declare_component(
     "file_manager", path=build_dir  # This uses the built frontend
 )
