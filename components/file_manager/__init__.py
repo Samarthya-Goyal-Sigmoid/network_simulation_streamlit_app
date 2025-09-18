e1d63fae2addf7d70a30d34225aaf9eafb5eb9fe
Samarthya Goyal <samarthya@Samarthyas-MacBook-Air.local>
2025-09-18 14:28:56 +0530

    Fix: Added homepage and rebuilt component

--- components/file_manager/__init__.py
+++ components/file_manager/__init__.py
@@ -5,6 +5,9 @@ import streamlit.components.v1 as components
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend", "build")
 
+
+print("📁 Custom component build path:", build_dir)
+
 # Declare the component
 file_manager_component = components.declare_component(
     "file_manager", path=build_dir  # This uses the built frontend
